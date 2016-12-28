618cf9197a679f0226d81ef779496bc5551c14e7
luoshi0429 <452039165@qq.com>
2016-12-28 17:58:45 +0800

    代码风格修改

--- src/view/order/OrderDetail.js
+++ src/view/order/OrderDetail.js
@@ -6,11 +6,11 @@ class OrderDetail extends Component {
     return (
       <div className='order-detail'>
         <NavBar leftBarClass='fa fa-chevron-left' title='寄件' />
-          <div className='input-container'>
-            <Input placeholder='请填写寄件人地址' className='order-input' />
-            <Input placeholder='请填写收件人地址' className='order-input' />
-            <Input placeholder='请填写物品类型' className='order-input' />
-          </div>
+        <div className='input-container'>
+          <Input placeholder='请填写寄件人地址' className='order-input' />
+          <Input placeholder='请填写收件人地址' className='order-input' />
+          <Input placeholder='请填写物品类型' className='order-input' />
+        </div>
         <div className='bottom-container'>
           <label><input type='checkbox' defaultChecked='true' />我已阅读并同意<a href='#'>《服务协议》</a></label>
           <button>我要寄件</button>
